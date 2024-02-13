1f97e01bc227fc5badc2d7de4fb13ba070fd02c1
omeralpi <omeralpi@icloud.com>
2024-02-14 00:29:30 +0300

    feat: add text highlighter component

---
+++ src/components/text-highlighter.tsx
@@ -0,0 +1,17 @@
+const TextHighlighter: React.FC<{ text: string; searchText: string }> = ({
+  text,
+  searchText,
+}) => {
+  const index = text.toLowerCase().indexOf(searchText.toLowerCase());
+  if (index === -1) return <>{text}</>;
+
+  return (
+    <>
+      {text.substring(0, index)}
+      <span style={{ fontWeight: "bold" }}>
+        {text.substring(index, index + searchText.length)}
+      </span>
+      {text.substring(index + searchText.length)}
+    </>
+  );
+};
